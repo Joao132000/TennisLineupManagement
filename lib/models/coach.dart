06474df754ba938aaf21e795773b46137ca6f40a
joao132000 <jpcscarvalho@gmail.com>
2023-01-27 20:14:43 -0600

    First

--- lib/models/coach.dart
+++ lib/models/coach.dart
@@ -1,22 +1,21 @@
-class Coach{
+class Coach {
   String id;
   final String name;
   final String email;
 
   Coach({
-    this.id='',
+    this.id = '',
     required this.name,
     required this.email,
   });
-  Map<String, dynamic>toJson()=>{
-    'id':id,
-    'name':name,
-    'email': email,
-  };
-  static Coach fromJson(Map<String,dynamic>json)=>Coach(
-    id: (json['id']??'').toString(),
-    name: (json['name']??'').toString(),
-    email: (json['email']??'').toString(),
-  );
-
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'email': email,
+      };
+  static Coach fromJson(Map<String, dynamic> json) => Coach(
+        id: (json['id'] ?? '').toString(),
+        name: (json['name'] ?? '').toString(),
+        email: (json['email'] ?? '').toString(),
+      );
 }
